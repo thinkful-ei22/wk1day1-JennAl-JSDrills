1521872342a312f1770ae12d60e0f07ce60e6014
Albert Fougy <albert@fougy.com>
2018-06-11 16:35:48 -0400

    adjusted age

--- drills.js
+++ drills.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const yearOfBirth = function (age){
+  return  2018-age;
+}
+
+
 const whoAmI = function (name, age){
-  let yearOfBirth = 2018-age;
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yearOfBirth}.`);
+  console.log(`I was born in ${yearOfBirth(age)}.`);
+
 };
+
+
+
 
 console.log(whoAmI('Jenn', 30));
