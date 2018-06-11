3fa7a69dbf01bd0acf091e80fc8e646ccdc7271f
Jennifer Colna <jennifer.colna@gmail.com>
2018-06-11 16:32:06 -0500

    to infinity

--- drills2.js
+++ drills2.js
@@ -1,9 +1,25 @@
 'use strict';
 
-const jediName = function(firstName,lastName){
-  let jediFirst = lastName.slice(0,3);
-  let jediLast = firstName.slice(0,2);
-  return jediFirst+jediLast;   
-};
+// const jediName = function(firstName,lastName){
+//   let jediFirst = lastName.slice(0,3);
+//   let jediLast = firstName.slice(0,2);
+//   return jediFirst+jediLast;   
+// };
 
-console.log(jediName('Jennifer','Colna'));
+// console.log(jediName('Jennifer','Colna'));
+
+const beyond = function(num){
+  if (Number.isFinite(num) && num>0){
+    console.log('To infinity');
+  }
+  else if (Number.isFinite(num) && num<0){
+    console.log('To negative infinity');
+  }
+  else if(num===0){
+    console.log('Staying home');
+  }
+  else {
+    console.log('And beyond');
+  }
+};
+beyond(0);
